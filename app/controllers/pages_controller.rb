cc3e92e0966759dc394f598236b2c4bb68c26f0a
Raelen Marchetti <raelenmarchetti@gmail.com>
2023-05-11 20:02:29 -0400

    implement a product listing page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,12 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
-  
-  def home
+
+  def index
+    @products = Product.paginate(page: params[:page], per_page: 10)
   end
+
+  def home
+
+  end
+
 end
