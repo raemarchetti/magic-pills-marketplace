c53fe0dd5feaad9a802cb2244104732ecf61df8b
Raelen Marchetti <raelenmarchetti@gmail.com>
2023-05-13 11:41:41 -0400

    seeds refactoring

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,18 +8,6 @@ user = User.create!(
   password: 'password'
 )
 
-pills = [
-  { name: 'Pill A', effect: 'Effect A', side_effects: 'Side Effect A', lifespan_days: 30, price: 9.99, user_id: 3 },
-  { name: 'Pill B', effect: 'Effect B', side_effects: 'Side Effect B', lifespan_days: 60, price: 19.99, user_id: 3 },
-  { name: 'Pill C', effect: 'Effect C', side_effects: 'Side Effect C', lifespan_days: 90, price: 29.99, user_id: 3 },
-  # Add more pills as needed
-]
-
-user = User.create!(
-  email: 'user@example.com',
-  password: 'password'
-)
-
 categories = ['Invisibility', 'Mind-Reading', 'Superhuman Strength', 'Tele-Transportation']
 categories.each do |category|
   5.times do
@@ -29,8 +17,7 @@ categories.each do |category|
       side_effects: Faker::Lorem.sentence,
       lifespan_days: Faker::Number.between(from: 30, to: 365),
       price: Faker::Number.decimal(l_digits: 2),
-      category: category,
-      user_id: user.id
+      category: category
     )
     product.user = user
     product.save!
