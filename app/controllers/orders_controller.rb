f72c2a45e664435050ad1f2848be6c61458412d8
Raelen Marchetti <raelenmarchetti@gmail.com>
2023-05-17 09:34:27 -0400

    add cart and refactoring

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,23 @@
 class OrdersController < ApplicationController
+  before_action :set_order_items, only: [:show]
+
   def index
     @orders = Order.select(:id, :created_at, :product_id, :user_id).all
   end
-  
-  def show
 
+  def show
+    #
   end
 
   def create
 
   end
+
+  private
+
+  def set_order_items
+    @order = current_user.order
+    @set_order_items ||= @order&.order_items
+  end
+
 end
