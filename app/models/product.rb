b49bc1d0affc7d24f910938384080cf3804d38ba
Raelen Marchetti <raelenmarchetti@gmail.com>
2023-05-19 22:12:54 -0400

    add cart to navbar and links

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,11 +2,11 @@ class Product < ApplicationRecord
   belongs_to :user
   has_many :order_items
 
-  # validates :name, presence: true
-  # validates :category, presence: true
-  # validates :effect, presence: true
-  # validates :lifespan_days, presence: true, numericality: { only_integer: true, greater_than: 0 }
-  # validates :side_effects, presence: true
-  # validates :price, presence: true, numericality: { greater_than: 0 }
-
+  validates :name, presence: true
+  validates :category, presence: true
+  validates :effect, presence: true
+  validates :lifespan_days, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :side_effects, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
+  
 end
